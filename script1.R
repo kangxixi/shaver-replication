fb7849515ac5b24685c2d74e9d27f84200c29f56
Phebo Wibbens <phebo.wibbens@insead.edu>
2020-06-03 14:52:54 +0200

    Run single model regressions

--- script1.R
+++ script1.R
@@ -1,10 +1,11 @@
 library(tidyverse)
+library(lfe)
 
 n <- 100
 Tt <- 5
 gamma <- rep(10, 5)
 
-set.seed(984590437)
+set.seed(98450437)
 u1 <- runif(n)
 u2 <- runif(n * Tt)
 u3 <- runif(n)
@@ -19,3 +20,17 @@ df <- expand_grid(i = 1:n, t = 1:Tt) %>%
     e = rnorm(n * Tt, mean = 0, sd = 1),
     y = 100 + 500 * x1 + 5 * x1 * x3 + 1000 * e)
 
+pairs(df %>% select(x1:y))
+model1 <- lm(y ~ x1 * x2 + factor(i) - 1, df)
+summary(model1)
+plot(model1)
+
+model2 <- felm(y ~ x1 * x2 | i, df)
+summary(model2)
+
+model3 <- felm(y ~ x1 * x2 | i + t | 0 | i, df)
+summary(model3)
+
+df <- df %>% mutate(r = as.vector(resid(model2)))
+ggplot(df, aes(x = x1, y = r, color = t)) +
+  geom_point()
